268e13af0fabe6952cd5d881b4f280154a4d81be
Ranjitha <ranjitha_omprasad@gap.com>
2021-01-25 13:09:12 -0800

    first commit

---
+++ src/main/java/com/RestApiMavenAzure/RestApiMavenAzureApplication.java
@@ -0,0 +1,13 @@
+package com.RestApiMavenAzure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiMavenAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiMavenAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RestApiMavenAzure/RestApiMavenAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestApiMavenAzure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiMavenAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
